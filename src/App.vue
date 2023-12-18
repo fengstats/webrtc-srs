5d4ff60264500f04df76bd895b868375a7816595
fengstats <feng2860984180@163.com>
2023-12-19 01:46:50 +0800

    feat: 通过 flv.js 播放 SRS 的媒体流

--- src/App.vue
+++ src/App.vue
@@ -2,82 +2,100 @@
   <h1 class="font-normal text-xl">浏览器通过 WebRTC 推流到 SRS 服务</h1>
 
   <div class="control">
-    <button @click="getUserMedia">获取媒体流</button>
+    <button class="m-r-2" @click="getUserMedia">获取本地媒体流并推送到 SRS 服务</button>
+    <button @click="playLiveMedia">通过 flv.js 播放 SRS 服务媒体流</button>
   </div>
-  <div v-if="!isSupportRTC" p="y-4" color="red">该浏览器不支持 RTCPeerConnection，请尝试切换 Chrome</div>
 
   <div m="t-4">
-    <video ref="localVideoRef" muted autoplay class="w-160 h-auto border-1 border-solid border-blue"></video>
+    <video ref="localVideoRef" muted autoplay class="w-150 h-120 border-2 border-solid border-blue"></video>
+    <video ref="remoteVideoRef" muted autoplay controls class="w-150 h-120 border-2 border-solid border-green"></video>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import axios from 'axios'
+import flvjs from 'flv.js'
 
-const isSupportRTC = ref<boolean>(true)
-
-// 1. 创建一个 RTCPeerConnection 对象
-const rtcPC = ref<RTCPeerConnection>()
-rtcPC.value = new RTCPeerConnection()
-if (!rtcPC.value) isSupportRTC.value = false
-
-// console.log(rtcPC.value)
-
+const appStreamUrl = ref('feng/testLive')
+const noSupportText = ref('暂不支持该浏览器，请尝试切换其他浏览器使用（如新版 Chrome）')
 const localVideoRef = ref<HTMLVideoElement>()
+const remoteVideoRef = ref<HTMLVideoElement>()
 const localStream = ref<MediaStream>()
+const rtcPC = ref<RTCPeerConnection>()
 
+// 获取
 const getUserMedia = async () => {
+  // 1. 创建一个 RTCPeerConnection 对象
+  rtcPC.value = new RTCPeerConnection()
+  console.warn('RTCPeerConnection')
+  console.log(rtcPC.value)
+  if (!rtcPC.value) {
+    alert(noSupportText.value)
+    return
+  }
+
   // 2. 获取媒体流返回 MediaStream 赋值给本地流
   const stream = await navigator.mediaDevices.getDisplayMedia({ audio: true, video: true })
   localStream.value = stream
   localVideoRef.value!.srcObject = stream
 
-  // 3. 获取本地流遍历轨道，插入到 rtcPC 里面
+  // 3. 获取本地媒体流视频 + 音频轨道列表，插入到 RTC 里面
   stream.getTracks().forEach((track) => {
-    // NOTE: 注意这里的本地流 stream 也要一起添加进去
-    rtcPC.value?.addTrack(track, stream)
+    // 这里的本地媒体流 stream 在第二个参数也要一起添加进去
+    // TODO: 貌似不需要也能正常走完流程，回头再看
+    rtcPC.value?.addTrack(track)
   })
 
-  // console.log(stream)
-
+  // 4. 与 SRS 交换信息
   handleNegotiation()
+}
+
+// 播放
+const playLiveMedia = () => {
+  if (!flvjs.isSupported()) {
+    alert(noSupportText.value)
+    return
+  }
+  const flvPlayer = flvjs.createPlayer({
+    type: 'flv',
+    url: `http://localhost:8080/${appStreamUrl.value}.flv`,
+  })
+  flvPlayer.attachMediaElement(remoteVideoRef.value!)
+  flvPlayer.load()
+  flvPlayer.play()
 }
 
 // 协商：交换会话描述信息 offer 和 answer
 const handleNegotiation = async () => {
-  // 1. 创建一个 offer session description
-  const offerSDP = await rtcPC.value?.createOffer()
-
+  // 1. 创建 offer sdp (session description)
+  const offer = await rtcPC.value?.createOffer()
   console.warn('offer sdp')
-  console.log(offerSDP) // { type: 'offer', sdp: 'xxx……' }
+  // { type: 'offer', sdp: 'xxx……' }
+  console.log(offer)
 
-  // 2. 将 offer 设置为 rtcPC 的本地描述
-  await rtcPC.value?.setLocalDescription(offerSDP)
+  // 2. 将 offer 设置为 RTC 的本地描述
+  await rtcPC.value?.setLocalDescription(offer)
 
-  // 3. 调用 API 交换 sdp
-  // 将 offer 发送给 SRS 服务
-  // NOTE: 被官网的这个地址误导了 http://192.168.1.170:1985/api/v1
-  // 以为 /rtc/v1/publish/ 要加上这个前缀地址才行，结果不用
+  // 3. 将 offer 发送给 SRS 服务
   const { data } = await axios.post('http://localhost:1985/rtc/v1/publish/', {
-    api: '/rtc/v1/publish/', // 不写也没事
-    streamurl: 'webrtc://localhost/feng/testShare',
-    sdp: offerSDP?.sdp,
+    api: '/rtc/v1/publish/', // 不写也行
+    streamurl: `webrtc://localhost:8080/${appStreamUrl.value}`,
+    sdp: offer?.sdp,
   })
   if (data.code !== 0) {
-    console.error('SRS 服务返回错误，请查看请求参数是否正确！')
+    console.error('SRS 服务返回错误，检查请求参数是否正确！')
     console.log(data)
     return
   }
 
-  // 把 SRS 回的 answer 设置成远程描述，至此，交换完成
-  // 创建一个 answer 对象
-  const answerSDP = new RTCSessionDescription({ type: 'answer', sdp: data.sdp })
+  // 4. 通过 SRS 返回的 sdp 信息创建 answer sdp 对象
+  const answer = new RTCSessionDescription({ type: 'answer', sdp: data.sdp })
   console.warn('answer sdp')
-  console.log(answerSDP)
+  console.log(answer)
 
-  rtcPC.value?.setRemoteDescription(answerSDP)
+  // 5. 将 answer 设置成远程描述
+  // 至此，ICE 交换流程完成！
+  rtcPC.value?.setRemoteDescription(answer)
 }
 </script>
-
-<style></style>
--- uno.config.ts
+++ uno.config.ts
@@ -3,10 +3,10 @@ import { defineConfig, presetMini, presetUno, presetWind, presetAttributify, pre
 export default defineConfig({
   // ...UnoCSS options
   presets: [
-    presetAttributify(), // 属性化模式，不用写全在 class 里面，可以按照属性类型分类，还节省了重复输入相同前缀的时间，甚至还有无值属性的支持
-    presetMini(), // 最小但是包含基本的规则和变量
-    // presetUno(), // 默认预设，包含了一系列流行的原子化框架的通用超集
+    presetUno(), // 默认预设，包含了一系列流行的原子化框架的通用超集
+    // presetMini(), // 最小但是包含基本的规则和变量
     // presetWind(), // Tailwind / Windi
+    presetAttributify(), // 属性化模式，不用写全在 class 里面，可以按照属性类型分类，还节省了重复输入相同前缀的时间，甚至还有无值属性的支持
     // presetIcons(), // 纯 CSS 图标，由 https://iconify.design/ 提供
   ],
 })
